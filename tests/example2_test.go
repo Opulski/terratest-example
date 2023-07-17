5bb8d6b9c3300f6afb72c9b03071baa94f190bb0
Ludwig Opulski <ludwig.opulski@gmail.com>
2023-07-17 16:44:50 +0200

    fix test name

--- tests/example2_test.go
+++ tests/example2_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
-func TestTerraformExample(t *testing.T) {
+func TestTerraformExample2(t *testing.T) {
 	// Construct the terraform options with default retryable errors to handle the most common
 	// retryable errors in terraform testing.
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
@@ -17,16 +17,10 @@ func TestTerraformExample(t *testing.T) {
 	// Clean up resources with "terraform destroy" at the end of the test.
 	defer terraform.Destroy(t, terraformOptions)
 
-	// Run "terraform init" and "terraform apply". Fail the test if there are any errors.
-	terraform.InitAndApply(t, terraformOptions)
-
-	// website::tag::2:: Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
-	terraform.InitAndApply(t, terraformOptions)
-
-	// website::tag::3:: Run `terraform output` to get the values of output variables
+	Run `terraform output` to get the values of output variables
 	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
 
-	// website::tag::4:: Verify the resource group exists
+	// Verify the resource group exists
 	exists := azure.ResourceGroupExists(t, resourceGroupName, subscriptionID)
 	assert.True(t, exists, "Resource group does not exist")
 }
